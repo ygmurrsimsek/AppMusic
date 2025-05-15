e266ae947107646c130e74eb2d9a1b490c3e26a2
ygmurrsimsek <ygmurrsimsek@icloud.com>
2025-05-15 17:22:29 +0300

    proje dizini oluşturuldu

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { StyleSheet, View,Switch,FlatList ,SafeAreaView, Text} from 'react-native';
+const AppMusic = () => {
+  return (
+  );
+}
+export default AppMusic;
