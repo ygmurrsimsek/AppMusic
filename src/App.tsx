92e02c6f6b2ed3d58e214c0a191cc73f0196ae5b
ygmurrsimsek <ygmurrsimsek@icloud.com>
2025-05-21 13:09:33 +0300

    klasör yapısı oluşturuldu.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { StyleSheet, View,Switch,FlatList ,SafeAreaView, Text} from 'react-native';
+
 const AppMusic = () => {
   return (
+      <SafeAreaView>
+
+      </SafeAreaView>
   );
 }
 export default AppMusic;
